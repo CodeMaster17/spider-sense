9f30d0ff1e3a3350f7f6faf8b9f56027270a19e9
Harshit Yadav <harshityadav172003@gmail.com>
2024-01-28 20:57:48 +0530

    ⚡️ feat: scraper and parsing functions

--- lib/scraper/index.ts
+++ lib/scraper/index.ts
@@ -2,7 +2,7 @@
 
 import axios from "axios";
 import * as cheerio from "cheerio";
-import { extractPrice } from "../utils";
+import { extractCurrency, extractDescription, extractPrice } from "../utils";
 export async function scrapeAmazonProduct(url: string) {
   if (!url) return;
 
@@ -44,9 +44,39 @@ export async function scrapeAmazonProduct(url: string) {
     const outOfStock =
       $("#availability span").text().trim().toLowerCase() ===
       "currently unavailable";
+    const images =
+      $("#imgBlkFront").attr("data-a-dynamic-image") ||
+      $("#landingImage").attr("data-a-dynamic-image") ||
+      "{}";
 
+    const imageUrls = Object.keys(JSON.parse(images));
 
-    console.log({ title, currentPrice, originalPrice });
+    const currency = extractCurrency($(".a-price-symbol"));
+    const discountRate = $(".savingsPercentage").text().replace(/[-%]/g, "");
+
+    const description = extractDescription($);
+
+    // Construct data object with scraped information
+    const data = {
+      url,
+      currency: currency || "$",
+      image: imageUrls[0],
+      title,
+      currentPrice: Number(currentPrice) || Number(originalPrice),
+      originalPrice: Number(originalPrice) || Number(currentPrice),
+      priceHistory: [],
+      discountRate: Number(discountRate),
+      category: "category",
+      reviewsCount: 100,
+      stars: 4.5,
+      isOutOfStock: outOfStock,
+      description,
+      lowestPrice: Number(currentPrice) || Number(originalPrice),
+      highestPrice: Number(originalPrice) || Number(currentPrice),
+      averagePrice: Number(currentPrice) || Number(originalPrice),
+    };
+    console.log(data);
+    return data;
   } catch (error: any) {
     console.log(error);
   }
